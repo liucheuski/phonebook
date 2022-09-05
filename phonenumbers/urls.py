7697959de4ec750ee3735f00385efbbf07d89ec2
liucheuski <liucheuski@gmail.com>
2022-09-05 21:54:48 +0300

    login, logout, change password

--- phonenumbers/urls.py
+++ phonenumbers/urls.py
@@ -5,6 +5,10 @@ from phonenumbers import views
 app_name = 'phonenumbers'
 urlpatterns = [
     path(r'', views.index, name='index'),
-    re_path(r'^([0-9]+)/$', views.details, name='details')
+    re_path(r'^([0-9]+)/$', views.details, name='details'),
+    re_path(r'^register/$', views.RegisterFormView.as_view()),
+    re_path(r'^login/$', views.LoginFormView.as_view()),
+    re_path(r'^logout/$', views.LogoutView.as_view()),
+    re_path(r'^password-change/', views.PasswordChangeView.as_view()),
 
 ]
--- phonenumbers/views.py
+++ phonenumbers/views.py
@@ -23,7 +23,7 @@ def index(request):
     )
 
 
-# страница загадки со списком ответов
+# страница загадки со списком номеров телефонов
 def details(request, name_id):
     error_message = None
     if "error_message" in request.GET:
@@ -36,3 +36,94 @@ def details(request, name_id):
             "error_message": error_message
         }
     )
+
+# Базовый класс для обработки страниц с формами.
+from django.views.generic.edit import FormView
+# Спасибо django за готовую форму регистрации.
+from django.contrib.auth.forms import UserCreationForm
+...
+# базовый URL приложения, главной страницы -
+# часто нужен при указании путей переадресации
+app_url = "/phonenumbers/"
+
+# наше представление для регистрации
+class RegisterFormView(FormView):
+    # будем строить на основе встроенной в django формы регистрации
+    form_class = UserCreationForm
+    # Ссылка, на которую будет перенаправляться пользователь
+    # в случае успешной регистрации.
+    # В данном случае указана ссылка на
+    # страницу входа для зарегистрированных пользователей.
+    success_url = app_url + "login/"
+    # Шаблон, который будет использоваться
+    # при отображении представления.
+    template_name = "reg/register.html"
+    def form_valid(self, form):
+        # Создаём пользователя,
+        # если данные в форму были введены корректно.
+        form.save()
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+# Спасибо django за готовую форму аутентификации.
+from django.contrib.auth.forms import AuthenticationForm
+# Функция для установки сессионного ключа.
+# По нему django будет определять,
+# выполнил ли вход пользователь.
+from django.contrib.auth import login
+...
+# наше представление для входа
+class LoginFormView(FormView):
+    # будем строить на основе
+    # встроенной в django формы входа
+    form_class = AuthenticationForm
+    # Аналогично регистрации,
+    # только используем шаблон аутентификации.
+    template_name = "reg/login.html"
+    # В случае успеха перенаправим на главную.
+    success_url = app_url
+    def form_valid(self, form):
+        # Получаем объект пользователя
+        # на основе введённых в форму данных.
+        self.user = form.get_user()
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+# Для Log out с перенаправлением на главную
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+...
+# для выхода - миниатюрное представление без шаблона -
+# после выхода перенаправим на главную
+class LogoutView(View):
+    def get(self, request):
+        # Выполняем выход для пользователя,
+        # запросившего данное представление.
+        logout(request)
+        # После чего перенаправляем пользователя на
+        # главную страницу.
+        return HttpResponseRedirect(app_url)
+
+
+# Для смены пароля - форма
+from django.contrib.auth.forms import PasswordChangeForm
+...
+# наше представление для смены пароля
+class PasswordChangeView(FormView):
+    # будем строить на основе
+    # встроенной в django формы смены пароля
+    form_class = PasswordChangeForm
+    template_name = 'reg/password_change_form.html'
+    # после смены пароля нужно снова входить
+    success_url = app_url + 'login/'
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        if self.request.method == 'POST':
+            kwargs['data'] = self.request.POST
+        return kwargs
+    def form_valid(self, form):
+        form.save()
+        return super(PasswordChangeView, self).form_valid(form)
